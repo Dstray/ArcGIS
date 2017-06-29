2961b6361c421ab48c4ccef2c22998924e0d188d
Dstray <lixt12@qq.com>
2017-06-29 12:45:29 -0500

    Search result initial style

--- main.js
+++ main.js
@@ -9,30 +9,34 @@ require([
         "esri/Map",
         "esri/PopupTemplate",
         "esri/geometry/Point",
+        "esri/layers/GraphicsLayer",
         "esri/tasks/Locator",
         "esri/symbols/PictureMarkerSymbol",
-        //"esri/symbols/SimpleMarkerSymbol",
+        "esri/symbols/SimpleMarkerSymbol",
         "esri/views/MapView",
         "esri/widgets/BasemapToggle",
         "esri/widgets/Expand",
         "esri/widgets/Locate",
         "esri/widgets/Search",
         "dojo/dom",
+        "dojo/dom-construct",
         "dojo/domReady!"
     ], function(
         Graphic,
         Map, 
         PopupTemplate,
         Point,
+        GraphicsLayer,
         Locator, 
         PictureMarkerSymbol,
-        //SimpleMarkerSymbol,
+        SimpleMarkerSymbol,
         MapView, 
         BasemapToggle, 
         Expand,
         Locate, 
         Search,
-        dom
+        dom,
+        domConstruct
     ){
 
     var map = new Map({
@@ -55,15 +59,30 @@ require([
         }
     });
 
+    // ------------- Expand widget ---------------
+    var qExpand = new Expand({
+        view: view,
+        content: dom.byId("query"),
+        expandIconClass: "esri-icon-drag-horizontal"
+    });
+    view.ui.add(qExpand, "top-right");
+
     // ------------- Point graphic ---------------
-    var pointGraphic = null;/*
-    var markerSymbol = new SimpleMarkerSymbol({
-        color: [226, 119, 40],
+    var pointGraphic = null;
+    var redMarkerSymbol = new SimpleMarkerSymbol({
+        color: [216, 0, 0],
         outline: {
             color: [255, 255, 255],
             width: 1
         }
-    });*/
+    });
+    var greyMarkerSymbol = new SimpleMarkerSymbol({
+        color: [144, 144, 144],
+        outline: {
+            color: [255, 255, 255],
+            width: 1
+        }
+    });
     var markerSymbol = new PictureMarkerSymbol({
         url: "./marker.png",
         width: 32,
@@ -83,6 +102,7 @@ require([
         url: "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"
     });
     view.on("click", function(event){
+        if (event.button != 0) return;
         view.popup.clear();
 
         /*var lat = Math.round(event.mapPoint.latitude * 10000) / 10000;
@@ -103,10 +123,11 @@ require([
             //console.log(response.address);
             //pointGraphic.attributes = response.address;
 
+            qExpand.collapse();
             view.popup.open({
                 title: response.address.Address,
                 content: "<p><b>Address</b>: " + response.address.Match_addr + "</p>" + 
-                    "<p><b>Coordinate</b>: [" + lon + ", " + lat + "]</p>",
+                    "<p><b>Coordinate</b>: [" + lat + ", " + lon + "]</p>",
                 location: event.mapPoint
             });
         }, function(error){
@@ -116,10 +137,10 @@ require([
 
     // ------------- Search widget ---------------
     var search = new Search({
-        view: view
+        //view: view
     });
     //search.defaultSource.withinViewEnabled = true; // Limit search to visible map area only
-    view.ui.add(search, "top-right"); // Add to the view
+    //view.ui.add(search, "top-right"); // Add to the view
 
     // ------------- Locate widget ---------------
     var curCoords = null;
@@ -147,11 +168,46 @@ require([
         position: "bottom-left"
     });
 
-    // ------------- Expand widget ---------------
-    var qExpand = new Expand({
-        view: view,
-        content: dom.byId("query"),
-        expandIconClass: "esri-icon-drag-horizontal"
-    });
-    view.ui.add(qExpand, "top-right");
+    // ------------- Search query ---------------
+    var resLayer = new GraphicsLayer();
+    var sinput = dom.byId("search-input");
+    var searchfunc = function(){
+        if (sinput.value === "") return;
+        console.log(sinput.value);/*
+        locator.addressToLocations({
+            address: {
+                "field_name": "380 New York St, Redlands, CA 92373"
+            }
+        }).then(function(cands){
+            console.log(cands);
+        });*/
+        resLayer.removeAll();
+        search.search(sinput.value).then(function(results){
+            results = results[0].results;
+            for (let res of results) {
+                if (res.feature.attributes.score < 90) continue;
+                res.feature.symbol = greyMarkerSymbol;
+                resLayer.add(res.feature);
+            }
+            
+            var resDiv = dom.byId("result-container");
+            for (var i = 0; i != resLayer.graphics.length; i++) {
+                var gr = resLayer.graphics.getItemAt(i);
+                domConstruct.create("div", {
+                    innerHTML: "<b>" + gr.attributes.Match_addr + "</b><p>" +
+                        gr.geometry.latitude + ", " + gr.geometry.longitude + "</p>",
+                    idx: i,
+                    class: "search-result-item"
+                }, resDiv);
+            }
+        });
+    };
+    dom.byId("search-form").onkeypress = function(event){
+        if ((event.keyCode || event.which || event.charCode || 0) === 13) {
+            searchfunc();
+            return false;
+        }
+    };
+    dom.byId("search-button").onclick = searchfunc;
+    map.add(resLayer);
 });
